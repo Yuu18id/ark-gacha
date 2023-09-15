3caad40e097a2d58b126ec3c9b02af09fc881c97
Yuu18id <211112007@students.mikroskil.ac.id>
2023-09-15 17:10:54 +0700

    add op img and clean code

--- index.js
+++ index.js
@@ -16,44 +16,37 @@ class Gacha {
             '2star': [],
             '1star': []
         };
-        this.output = document.querySelector("p");
-        this.paragraph = document.createElement("p");
+        this.result = document.querySelector(".result")
         this.dataFetched = false;
     }
 
     async fetchData() {
         if (!this.dataFetched) {
-            return fetch('https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/json/gamedata/en_US/gamedata/excel/character_table.json')
-            .then(response => response.json())
-            .then(data => {
+            try {
+                const response = await fetch('https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/json/gamedata/en_US/gamedata/excel/character_table.json');
+                const data = await response.json();
                 this.processData(data);
                 this.dataFetched = true;
-            })
-            .catch(error => {
+            } catch (error) {
                 console.error(error);
-            });
+            }
         }
     }
 
     processData(data) {
-        for (var i in data) {
-            if (data[i].rarity == 5 && data[i].description != null && data[i].subProfessionId != "notchar1" && data[i].subProfessionId != "notchar2" && data[i].isNotObtainable == false) {
-                this.op['6star'].push(data[i].name)
-            }
-            else if (data[i].rarity == 4 && data[i].description != null && data[i].subProfessionId != "notchar1" && data[i].subProfessionId != "notchar2" && data[i].isNotObtainable == false) {
-                this.op['5star'].push(data[i].name)
-            }
-            else if (data[i].rarity == 3 && data[i].description != null && data[i].subProfessionId != "notchar1" && data[i].subProfessionId != "notchar2" && data[i].isNotObtainable == false) {
-                this.op['4star'].push(data[i].name)
-            }
-            else if (data[i].rarity == 2 && data[i].description != null && data[i].subProfessionId != "notchar1" && data[i].subProfessionId != "notchar2" && data[i].isNotObtainable == false) {
-                this.op['3star'].push(data[i].name)
-            }
-            else if (data[i].rarity == 1 && data[i].description != null && data[i].subProfessionId != "notchar1" && data[i].subProfessionId != "notchar2" && data[i].isNotObtainable == false) {
-                this.op['2star'].push(data[i].name)
-            }
-            else if (data[i].rarity == 0 && data[i].description != null && data[i].subProfessionId != "notchar1" && data[i].subProfessionId != "notchar2" && data[i].isNotObtainable == false) {
-                this.op['1star'].push(data[i].name)
+        for (const key in data) {
+            if (key.startsWith("char")) {
+                const charData = data[key];
+                if (
+                    charData.rarity >= 1 &&
+                    charData.rarity <= 6 &&
+                    charData.description !== null &&
+                    charData.subProfessionId !== "notchar1" &&
+                    charData.subProfessionId !== "notchar2" &&
+                    charData.isNotObtainable === false
+                ) {
+                    this.op[charData.rarity + 'star'].push({ "name": charData.name, "img": key });
+                }
             }
         }
     }
@@ -61,47 +54,53 @@ class Gacha {
     async gacha(pull) {
         await this.fetchData();
 
-        if (this.output) {
-            this.output.remove();
-        }
-
-        this.paragraph.textContent = ''
+        // Menghapus semua elemen div dengan kelas "res" yang sudah ada
+        const existingRes = document.querySelectorAll(".res");
+        existingRes.forEach(element => element.remove());
 
         for (let i = 0; i < pull; i++) {
-            var rate = Math.random() * 100;
+            const div = document.createElement("div");
+            div.className = "res mx-1";
+            const rate = Math.random() * 100;
+            let rarity = 1;
+
             if (rate < 2) {
-                var get = this.op["6star"][Math.floor(Math.random() * this.op["6star"].length)];
-                this.own["6star"].push(get);
-                this.paragraph.textContent += `★★★★★★\t ${get}\n`;
+                rarity = 6;
+            } else if (rate < 16) {
+                rarity = 5;
+            } else if (rate < 51) {
+                rarity = 4;
+            } else {
+                rarity = 3;
             }
-            else if (rate > 1 && rate < 16) {
-                var get = this.op["5star"][Math.floor(Math.random() * this.op["5star"].length)];
-                this.own["5star"].push(get);
-                this.paragraph.textContent += `★★★★★\t${get}\n`;
-            }
-            else if (rate > 15 && rate < 51) {
-                var get = this.op["4star"][Math.floor(Math.random() * this.op["4star"].length)];
-                this.own["4star"].push(get);
-                this.paragraph.textContent += `★★★★\t${get}\n`;
-            }
-            else if (rate > 50 && rate < 100) {
-                var get = this.op["3star"][Math.floor(Math.random() * this.op["3star"].length)];
-                this.own["3star"].push(get);
-                this.paragraph.textContent += `★★★\t${get}\n`;
-            }
-            document.body.appendChild(this.paragraph)
+
+            const randomStar = this.op[rarity + "star"][Math.floor(Math.random() * this.op[rarity + "star"].length)];
+            const get = { "name": randomStar["name"], "img": randomStar["img"] };
+            this.own[rarity + "star"].push(get);
+
+            const img = document.createElement("img");
+            img.src = "https://raw.githubusercontent.com/Aceship/Arknight-Images/main/portraits/" + randomStar["img"] + "_1.png";
+            img.width = 90;
+            img.height = 160;
+
+            const paragraph = document.createElement("p");
+            paragraph.textContent = "★" + rarity + "\t" + get["name"] + "\n";
+
+            div.appendChild(img);
+            div.appendChild(paragraph);
+            this.result.appendChild(div);
         }
     }
 
     inventory() {
         const existP = document.body.querySelector('p');
         if (existP) {
-            existP.remove()
+            existP.remove();
         }
 
         const invenP = document.createElement("p");
-        invenP.textContent = JSON.stringify(this.own)
-        document.body.appendChild(invenP)
+        invenP.textContent = JSON.stringify(this.own);
+        document.body.appendChild(invenP);
     }
 }
 
